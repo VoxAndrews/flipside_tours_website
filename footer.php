2b580d89e0ca489535f02a63dc1cf43d5177b392
VoxAndrews <VoxAndrews@users.noreply.github.com>
2021-05-31 20:15:34 +0800

    Added PHP Files
    Added PHP Files to add to WordPress version of the Site

---
+++ footer.php
@@ -0,0 +1,58 @@
+<footer class="footerbg container-fluid">
+        <div class="container remove-padding content-width">
+            <div class="row" style="margin-bottom: 60px;">
+                <div class="col-xs-9 remove-padding">
+                    <div class="row">
+                        <div class="col-xs-12 remove-padding">
+                            <p><span style="font-family: 'Archivo Black', sans-serif;">Call</span> <span class="tab">+61 08 6939 6100</span></p>
+                            <p><span style="font-family: 'Archivo Black', sans-serif;">Email</span> <span class="tab">info@flipside.com</span></p>
+                        </div>
+                        <!--Column Medium 2-->
+
+                    </div>
+                    <!--Row-->
+                    <p style="padding-bottom: 45px;">2, Land Road, Fremantle, WA, 6160</p>
+                </div>
+                <!--Column Medium 6-->
+
+                <div class="col-sm-3 remove-padding footer-btns">
+                    <div class="row" style="padding-top: 0px;">
+                        <div class="col-xs-6" style="padding-left: 0px;">
+                            <p><a href="index.html">Home</a></p>
+                            <p><a href="services.html">Tours</a></p>
+                            <p><a href="blog.html">News</a></p>
+                        </div>
+                        <!--Column Medium 6-->
+
+                        <div class="col-xs-6 remove-padding">
+                            <p><a href="#">About Us</a></p>
+                            <p><a href="#">Contact Us</a></p>
+                        </div>
+                        <!--Column Medium 6-->
+                    </div>
+                    <!--Row-->
+                </div>
+                <!--Column Medium 6-->
+            </div>
+            <!--Row-->
+
+            <section class="container remove-padding content-width">
+                <div class="row">
+                    <div class="col-xs-1" style="padding-left: 0px;">
+                        <a href="index.html"><img alt="Flipside Tours Logo" src="http://206.189.45.97/~mesh22/sub4/wp-content/themes/flipside_tours_website/graphics/logo/logotype-img.svg"></a>
+                    </div>
+                    <!--Column Medium 6-->
+
+                    <div class="col-sm-11 remove-padding" style="text-align: right;">
+                        <p class="footer-legal">Flipside Tours | Copyright 2021 | All Rights Reserved</p>
+                    </div>
+                    <!--Column Medium 6-->
+                </div>
+                <!--Row-->
+            </section>
+        </div>
+    </footer>
+</body>
+
+</html>
+<?php wp_footer(); ?>
---
+++ header.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php
+		      if (function_exists('is_tag') && is_tag()) {
+		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+		      elseif (is_archive()) {
+		         wp_title(''); echo ' Archive - '; }
+		      elseif (is_search()) {
+		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		         wp_title(''); echo ' - '; }
+		      elseif (is_404()) {
+		         echo 'Not Found - '; }
+		      if (is_home()) {
+		         bloginfo('name'); echo ' - '; bloginfo('description'); }
+		      else {
+		          bloginfo('name'); }
+		      if ($paged>1) {
+		         echo ' - page '. $paged; }
+		   ?>
+    </title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Google Fonts (Roboto & Archivo Black) -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:ital,wght@0,300;0,400;0,900;1,700&display=swap" rel="stylesheet">
+
+    <!-- Stylesheet -->
+    <link href="style.css" rel="stylesheet">
+    
+<?php wp_head(); ?> <!—very important that this is added-->
+</head>
+
+<body>
+    <header class="container-fluid indexheaderbg">
+        <nav class="navbar content-width">
+            <div class="container-fluid remove-padding">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <a class="navbar-brand" href="index.html"><img alt="Flipside Tours Logo" src="http://206.189.45.97/~mesh22/sub4/wp-content/themes/flipside_tours_website/graphics/logo/logotype-img.svg"></a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li><a href="services.html">Tours</a></li>
+                        <li><a href="blog.html">News</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container remove-padding content-width">
+            <div class="row tagline-header content-align">
+                <div class="col-md-6 remove-padding">
+                    <h1> Fremantle <span style="font-family: 'Archivo Black', sans-serif;">After Dark</span> </h1>
+                    <!--Main Tag Line-->
+
+                    <p>Share in the fun of the local Fremantle Culture, and discover new and exciting experiences through our tours!</p>
+
+                    <button type="button" class="btn">View Now</button>
+                </div>
+                <!--Column Medium 6-->
+
+                <div class="col-md-6">
+
+                </div>
+                <!--Column Medium 6 (Empty)-->
+            </div>
+            <!--Row-->
+        </div>
+    </header>
