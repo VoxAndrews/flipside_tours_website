40d2109caf254c03b5142d332fdb5d030ad54d02
VoxAndrews <VoxAndrews@users.noreply.github.com>
2021-06-01 14:46:51 +0800

    Create home_page_template.php

---
+++ home_page_template.php
@@ -0,0 +1,60 @@
+<?php
+    /*
+    Template Name: Home
+    */
+?>
+
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+    <section class="container-fluid index-section-one-bg">
+        <section style="text-align:center;">
+            <h2 style="padding-top: 20px;">Want to see our <span style="font-family: 'Archivo Black', sans-serif;">Tours?</span></h2>
+            <p>Find our full list of activities and outings on our <span style="font-family: 'Roboto', sans-serif; font-weight: 700; font-style: italic;">Tour Page</span></p>
+        </section>
+
+        <section class="container content-width">
+            <div class="row">
+                <div class="col-sm-6 remove-padding index-tour-left-one">
+                    <h3><u> Ghostly <span style="font-family: 'Archivo Black', sans-serif;">Tours</span> </u></h3>
+                    
+                    <a href="services.html"><button type="button" class="btn">View Now</button></a>
+                </div>
+                
+                <div class="col-sm-6 remove-padding">
+                    <img class="index-tour-img" src="http://206.189.45.97/~mesh22/sub4/wp-content/themes/flipside_tours_website/images/content/index/index-tour-img-1.png" alt="Photo of the Whalers Tunnel in Fremantle">
+                </div>
+            </div>
+        </section>
+        
+        <section class="container content-width">
+            <div class="row">
+                <div class="col-sm-6 remove-padding index-tour-left-two">
+                    <h3><u> Evening Food <span style="font-family: 'Archivo Black', sans-serif;">Tours</span> </u></h3>
+                    
+                    <a href="services.html"><button type="button" class="btn">View Now</button></a>
+                </div>
+                
+                <div class="col-sm-6 remove-padding">
+                    <img class="index-tour-img" src="http://206.189.45.97/~mesh22/sub4/wp-content/themes/flipside_tours_website/images/content/index/index-tour-img-2.png" alt="Photo of the Spice Market in Fremantle">
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <section class="container-fluid index-section-two-bg">
+        <section class="container index-content-one remove-padding content-width">
+            <div class="row">
+                <div class="col-sm-4 remove-padding" style="padding-top: 45px">
+                    <h2>Want to keep <span style="font-family: 'Archivo Black', sans-serif;">up-to-date?</span></h2>
+
+                    <p>Check out our News Blog, and find out what's going on with us in and around Fremantle!</p>
+
+                    <a href="blog.html"><button type="button" class="btn">View Now</button></a>
+                </div>
+
+                <div class="col-sm-8 remove-padding" style="text-align: right">
+                    <img class="index-blog-img" src="http://206.189.45.97/~mesh22/sub4/wp-content/themes/flipside_tours_website/images/content/index/index-blog-img-1.jpg" alt="Photo of the Entrance to the Fremantle Prison">
+                </div>
+            </div>
+        </section>
+    </section>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
