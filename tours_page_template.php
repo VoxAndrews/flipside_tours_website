71ddbafe882bac4a7ec8f137fa41a980b57a930e
VoxAndrews <VoxAndrews@users.noreply.github.com>
2021-06-05 15:04:16 +0800

    Update tours_page_template.php
    Finished the Tours Page

--- tours_page_template.php
+++ tours_page_template.php
@@ -5,42 +5,59 @@
 ?>
 
 <?php include (TEMPLATEPATH . '/small_header.php');  /* Tells WordPress to include the smaller header header.php */ ?>
-<div class="container-fluid maincontainer">
-    <div class="container">
-        <div class="row">
-            <section class="col-sm-7 maintext">
-                <article class="excerpts">
-                    <!---->
-                    <!--we can reuse the css on the old article to restyle the new dynamic posts-->
+    <section class="container-fluid services-section-one-bg">
+        <section class="container content-width remove-padding">
+            <h1 style="text-align: center"><u style="text-decoration-thickness: 1px;"> <?php the_field('tourspagetitleone'); ?> </u></h1>
+            
+            <div class="row">
+                <div class="col-sm-4 remove-padding services-tour-left-one">
+                    <h3><u> <?php the_field('tourtitleone'); ?> </u></h3>
 
-                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <container>
+                        <p> <?php the_field('tourstarttimeone'); ?> </p>
+                        <p> <?php the_field('tourdurationone'); ?> </p>
+                        <p> <?php the_field('tourpriceone'); ?> </p>
+                    </container>
 
-                    <div class="post single-page">
-                        <h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
+                    <a href="<?php the_field('tourbuttonone'); ?>"><button type="button" class="btn">Book Now</button></a>
+                </div>
 
-                        <div class="postcontent">
-                            <?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-                            <?php the_content(); ?>
-                            <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
-                        </div>
-                        <!--postcontet-->
+                <div class="col-sm-4 remove-padding">
+                    <img class="services-img" src="<?php the_field('tourimageone'); ?>" alt="Photo of the Whalers Tunnel in Fremantle">
 
-                    </div>
-                    <!--post class-->
+                    <p> <?php the_field('tournoticeone'); ?> </p>
+                </div>
 
-                    <?php endwhile; ?>
+                <div class="col-sm-4 remove-padding services-tour-right-one">
+                    <p> <?php the_field('tourdetailsone'); ?> </p>
+                </div>
+            </div>
+        </section>
 
-                    <?php else: ?>
-                    <div class="post">
-                        <p>Sorry, no posts found.</p>
-                    </div>
-                    <!--post-->
-                    <?php endif; ?>
-                </article>
-                <!---->
-            </section>
+        <section class="container content-width remove-padding">
+            <div class="row">
+                <div class="col-sm-4 remove-padding services-tour-left-two">
+                    <h3><u> <?php the_field('tourtitletwo'); ?> </u></h3>
+                    
+                    <container>
+                        <p> <?php the_field('tourstarttimetwo'); ?> </p>
+                        <p> <?php the_field('tourdurationtwo'); ?> </p>
+                        <p> <?php the_field('tourpricetwo'); ?> </p>
+                    </container>
 
-        </div>
-    </div>
-</div>
+                    <a href="<?php the_field('tourbuttontwo'); ?>"><button type="button" class="btn">Book Now</button></a>
+                </div>
+
+                <div class="col-sm-4 remove-padding">
+                    <img class="services-img" src="<?php the_field('tourimagetwo'); ?>" alt="Photo of the Spice Market in Fremantle">
+                    
+                    <p> <?php the_field('tournoticetwo'); ?> </p>
+                </div>
+
+                <div class="col-sm-4 remove-padding services-tour-right-two">
+                    <p> <?php the_field('tourdetailstwo'); ?> </p>
+                </div>
+            </div>
+        </section>
+    </section>
 <?php get_footer(); ?>
